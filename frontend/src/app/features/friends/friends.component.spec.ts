1a86c5d7bf06c59c0e2218120828cec71463221a
nathan <106931831+Iceprod8@users.noreply.github.com>
2025-11-30 16:00:48 +0100

    Add test to ci/cd

---
+++ frontend/src/app/features/friends/friends.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { FriendsComponent } from './friends.component';
+
+describe('FriendsComponent', () => {
+  let component: FriendsComponent;
+  let fixture: ComponentFixture<FriendsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FriendsComponent, HttpClientTestingModule, RouterTestingModule]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(FriendsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
